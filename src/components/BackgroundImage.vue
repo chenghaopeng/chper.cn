04834e02bc0a53af9a2fe85b4cd9c8d5dd0399d2
chenghaopeng <i@chper.cn>
2021-01-31 21:07:47 +0800

    背景

---
+++ src/components/BackgroundImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <teleport to="body">
+    <div :class="$style.whole">
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core'
+
+export default defineComponent({
+  name: 'BackgroundImage'
+})
+</script>
+
+<style lang="less" module>
+.whole {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: url(../assets/bg.svg);
+  background-size: cover;
+  background-position: 50% 50%;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BackgroundImage from './components/BackgroundImage.vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('BackgroundImage', BackgroundImage)
+  .mount('#app')
