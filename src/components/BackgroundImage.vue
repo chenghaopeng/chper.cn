cbcf7cf34ec1e9423a85e461eb3da2ad2606b3bd
chenghaopeng <i@chper.cn>
2021-01-31 21:32:45 +0800

    背景自动模糊

--- src/components/BackgroundImage.vue
+++ src/components/BackgroundImage.vue
@@ -14,14 +14,31 @@ export default defineComponent({
 </script>
 
 <style lang="less" module>
-.whole {
+.whole, .whole::after {
   position: fixed;
   left: 0;
   top: 0;
   width: 100vw;
   height: 100vh;
+}
+.whole {
   background-image: url(../assets/bg.svg);
   background-size: cover;
   background-position: 50% 50%;
+  z-index: -1;
+}
+.whole::after {
+  content: '';
+  backdrop-filter: blur(64px);
+  opacity: 0;
+  animation-name: fadein;
+  animation-delay: 2s;
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+  animation-timing-function: cubic-bezier(0.32, 0, 0.67, 0);
+}
+@keyframes fadein {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
 }
 </style>
