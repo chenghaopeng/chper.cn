d993d3dfadc76048e29013f1ed5a402c89be88c1
chenghaopeng <i@chper.cn>
2021-02-13 16:10:49 +0800

    body

--- src/utils/tab.ts
+++ src/utils/tab.ts
@@ -2,7 +2,7 @@ const openInNewTab = (href: string) => {
   const a = document.createElement('a')
   a.href = href
   a.target = '_blank'
-  document.getElementsByTagName('body')[0].appendChild(a)
+  document.body.appendChild(a)
   a.click()
   a.remove()
 }
